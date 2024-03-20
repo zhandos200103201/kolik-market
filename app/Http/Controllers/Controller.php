d707ef57372e20a61f9f7241aafabb44c652a58a
Zhandos Saparbayev <200103201@stu.sdu.edu.kz>
2024-03-20 14:06:17 +0500

    added swagger documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      title="kolik-market.kz",
+ *      version="1.0",
+ *      description="",
+ *
+ *      @OA\Contact(
+ *          email="200103201@stu.sdu.edu.kz"
+ *      ),
+ * )
+ */
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, ResponseTrait, ValidatesRequests;
--- app/kolik/Domains/Controllers/Manufacturer/Controller.php
+++ app/kolik/Domains/Controllers/Manufacturer/Controller.php
@@ -13,6 +13,29 @@ use Illuminate\Support\Facades\Auth;
 
 final class Controller extends BaseController
 {
+    /**
+     * @OA\Get(
+     *      summary="Get Course Attendance records.",
+     *      path="/1.0.0/academic/courses/attendances",
+     *      operationId="1.0.0-academic-course-attendance-index",
+     *      tags={"academic", "1.0.0", "course", "attendance"},
+     *      description="Retrieves Course Attendance records.",
+     *      parameters={
+     *       {"name": "Authorization", "in": "header", "type": "string", "required": true, "description": "Bearer token"},
+     *       {"name": "course_code", "in": "query", "type": "string", "required": true, "example": "CSS 101", "description": "Course Code"},
+     *       {"name": "year", "in": "query", "type": "integer", "required": true, "example": "2022", "description": "Year"},
+     *       {"name": "term", "in": "query", "type": "integer", "required": true, "example": "2", "description": "Term"},
+     *       {"name": "section", "in": "query", "type": "string", "required": true, "example": "01", "description": "Section"}
+     *      },
+     *      security={{"passport": {}, "permissions": {"53", "132"}}},
+     *
+     *      @OA\Response(
+     *           response=200,
+     *           description="Attendance records are successfully returned.",
+     *
+     *      )
+     *  )
+     */
     public function index(): JsonResponse
     {
         return response()->json([
