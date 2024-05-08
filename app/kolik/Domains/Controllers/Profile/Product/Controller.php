0d7727c632fc2ab19f6ec3405b6ced970d1f13a4
Zhandos Saparbayev <200103201@stu.sdu.edu.kz>
2024-05-08 13:39:47 +0500

    add manufacturer and model id

--- app/kolik/Domains/Controllers/Profile/Product/Controller.php
+++ app/kolik/Domains/Controllers/Profile/Product/Controller.php
@@ -185,7 +185,8 @@ final class Controller extends BaseController
             'is_used' => $dto->isUsed,
             'category_id' => $dto->category_id,
             'user_id' => $userId,
-
+            'model_id' => $dto->model_id,
+            'manufacturer_id' => $dto->manufacturer_id,
         ]);
 
         return $this->response(
